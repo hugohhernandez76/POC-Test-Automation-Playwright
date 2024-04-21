e22a72d4d4b1ba26778101d55787a82ee5904e25
Victor Hernandez <84982212+hugohhernandez76@users.noreply.github.com>
2024-04-21 08:28:18 -0600

    workflow changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['allure-playwright']],
+  reporter: [['html'], ['allure-playwright', {outputFolder: 'test-results'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   globalSetup: require.resolve('./utils/globalSetup.ts'),
   use: {
